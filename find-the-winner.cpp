dc20c906f43422c040bc9df4eab61dd97399f17e
Hargun Kaur <hargunkaur286@gmail.com>
2024-12-31 23:32:14 +0530

    day1 :)

---
+++ find-the-winner.cpp
@@ -0,0 +1,18 @@
+//link -> https://leetcode.com/problems/find-the-winner-of-the-circular-game/submissions/1493529120/
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            a[i] = i+1;
+        }
+        int start = 0;
+        while (a.size() > 1) {
+            start = (start + (k - 1)) % a.size();
+            a.erase(a.begin() + start);
+            if (start == a.size()) start = 0;
+        }
+        return a[0];
+    }
+};
